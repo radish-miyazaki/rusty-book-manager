453be0afd224b075430e1eef7b30dc292769ef43
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-10-07 21:51:37 +0900

    ♻️ adopts layed architecture

---
+++ src/bin/app.rs
@@ -0,0 +1,33 @@
+use std::net::{Ipv4Addr, SocketAddr};
+
+use anyhow::{Error, Result};
+use axum::Router;
+use tokio::net::TcpListener;
+
+use adapter::database::connect_database_with;
+use api::route::health::build_health_check_routes;
+use registry::AppRegistry;
+use shared::config::AppConfig;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    bootstrap().await
+}
+
+async fn bootstrap() -> Result<()> {
+    let app_config = AppConfig::new()?;
+    let pool = connect_database_with(&app_config.database);
+
+    let registry = AppRegistry::new(pool);
+
+    let app = Router::new()
+        .merge(build_health_check_routes())
+        .with_state(registry);
+
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(addr).await?;
+
+    println!("Listening on {}", addr);
+
+    axum::serve(listener, app).await.map_err(Error::from)
+}
