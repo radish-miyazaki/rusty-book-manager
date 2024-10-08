00838464c6758c0318fc2aa3072247b4071910ea
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-10-08 23:06:25 +0900

    ✨ add application error struct

---
+++ shared/src/error.rs
@@ -0,0 +1,65 @@
+use axum::{http::StatusCode, response::IntoResponse};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum AppError {
+    #[error("{0}")]
+    UnprocessableEntity(String),
+    #[error("{0}")]
+    EntityNotFound(String),
+    #[error("{0}")]
+    ValidationError(#[from] garde::Report),
+    #[error("Does not transaction.")]
+    // INFO: #[from] には同じ型のエラーを指定できないため、#[source] で代用している
+    // @see https://github.com/dtolnay/thiserror/issues/123
+    TransactionError(#[source] sqlx::Error),
+    #[error("Error occurred in processing the database operation.")]
+    SpecificOperationError(#[source] sqlx::Error),
+    #[error("No rows affected: {0}")]
+    NoRowsAffectedError(String),
+    #[error("{0}")]
+    KeyValueStoreError(#[from] redis::RedisError),
+    #[error("{0}")]
+    BcyptError(#[from] bcrypt::BcryptError),
+    #[error("{0}")]
+    ConvertToUuidError(#[from] uuid::Error),
+    #[error("Failed to login.")]
+    UnauthenticatedError,
+    #[error("Invalid authorization information.")]
+    UnauthorizedError,
+    #[error("Forbidden operation.")]
+    ForbiddenError,
+    #[error("{0}")]
+    ConversionEntityError(String),
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        let status_code = match self {
+            AppError::UnprocessableEntity(_) => StatusCode::UNPROCESSABLE_ENTITY,
+            AppError::EntityNotFound(_) => StatusCode::NOT_FOUND,
+            AppError::ValidationError(_) | AppError::ConvertToUuidError(_) => {
+                StatusCode::BAD_REQUEST
+            }
+            AppError::UnauthenticatedError | AppError::ForbiddenError => StatusCode::UNAUTHORIZED,
+            AppError::UnauthorizedError => StatusCode::UNAUTHORIZED,
+            e @ (AppError::TransactionError(_)
+            | AppError::SpecificOperationError(_)
+            | AppError::NoRowsAffectedError(_)
+            | AppError::KeyValueStoreError(_)
+            | AppError::BcyptError(_)
+            | AppError::ConversionEntityError(_)) => {
+                tracing::error!(
+                    error.cause_chain = ?e,
+                    error.message = %e,
+                    "Unexpected error happend"
+                );
+                StatusCode::INTERNAL_SERVER_ERROR
+            }
+        };
+
+        status_code.into_response()
+    }
+}
+
+pub type AppResult<T> = Result<T, AppError>;
