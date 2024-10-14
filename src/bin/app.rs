c251c4705245d01857aaf132b5563e18d38eec03
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-10-14 19:50:53 +0900

    ✨ intro opentelemetry

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -5,7 +5,17 @@ use std::{
 
 use anyhow::{Context, Result};
 use axum::{http::Method, Router};
+use opentelemetry::{global, trace::TracerProvider, KeyValue};
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::Resource;
 use tokio::net::TcpListener;
+use tower_http::{
+    cors::{self, CorsLayer},
+    trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer},
+    LatencyUnit,
+};
+use tracing::Level;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 use adapter::{database::connect_database_with, redis::RedisClient};
 use api::route::{auth, v1};
@@ -14,13 +24,6 @@ use shared::{
     config::AppConfig,
     env::{which, Environment},
 };
-use tower_http::{
-    cors::{self, CorsLayer},
-    trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer},
-    LatencyUnit,
-};
-use tracing::Level;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[cfg(debug_assertions)]
 use api::openapi::ApiDoc;
@@ -41,20 +44,38 @@ fn init_logger() -> Result<()> {
         Environment::Production => "info",
     };
 
+    let host = std::env::var("JAEGER_HOST")?;
+    let port = std::env::var("JAEGER_PORT")?;
+    let endpoint = format!("http://{}:{}", host, port);
+
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint(endpoint),
+        )
+        .with_trace_config(opentelemetry_sdk::trace::Config::default().with_resource(
+            Resource::new(vec![KeyValue::new("service.name", "book-manager")]),
+        ))
+        .install_batch(opentelemetry_sdk::runtime::Tokio)?
+        .tracer_builder("book-manager-tracer")
+        .build();
+
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
 
     let subscriber = tracing_subscriber::fmt::layer()
         .with_file(true)
         .with_line_number(true)
-        .with_target(false);
-
-    // リリースビルドでのみ構造化ログを出力する
-    #[cfg(not(debug_assertions))]
-    let subscriber = subscriber.json();
+        .with_target(false)
+        .json();
 
     tracing_subscriber::registry()
         .with(subscriber)
         .with(env_filter)
+        .with(opentelemetry)
         .try_init()?;
 
     Ok(())
@@ -100,6 +121,7 @@ async fn bootstrap() -> Result<()> {
     tracing::info!("Listening on {}", addr);
 
     axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .context("Un expected error happened in server")
         // 起動失敗時のエラーログを tracing::error! マクロで出力
@@ -110,4 +132,39 @@ async fn bootstrap() -> Result<()> {
                 "Un expected error"
             )
         })
+}
+
+async fn shutdown_signal() {
+    fn purge_spans() {
+        global::shutdown_tracer_provider();
+    }
+
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to install CTRL+C signal handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("Failed to install SIGTERM signal handler")
+            .recv()
+            .await
+            .expect("Failed to receive SIGTERM signal");
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending();
+
+    tokio::select! {
+        _ = ctrl_c => {
+            tracing::info!("Ctrl-C を受診しました。");
+            purge_spans();
+        },
+        _ = terminate => {
+            tracing::info!("SIGTERM を受診しました。");
+            purge_spans();
+        }
+    }
 }
